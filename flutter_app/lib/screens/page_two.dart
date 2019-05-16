d72b65bccfbf983ae6710f7cddfda5d1fe38cb21
ifackerx <ifackerx@gmail.com>
2019-05-17 00:52:01 +0700

    col&row :chocolate_bar:

--- flutter_app/lib/screens/page_two.dart
+++ flutter_app/lib/screens/page_two.dart
@@ -8,9 +8,38 @@ class PageTwo extends StatefulWidget {
 class _PageTwoState extends State<PageTwo> {
   @override
   Widget build(BuildContext context) {
-    return Center( 
-      child: Text('Page 2'),
-      
-    );
+    return ListView(
+      children: <Widget>[
+        Column(
+      // กำหนด ตำแหน่ง
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Image(image: AssetImage('asset/img/profile.jpg')),
+        Container(height: 100.0, color: Colors.pink),
+        Image(image: AssetImage('asset/img/profile.jpg')),
+
+        Row(
+          children: <Widget>[
+            // ถ้าใช้ Row ภายใจ้ widget ตัวอื่นต้อง expand
+            Expanded(
+             child: Container(
+                height: 100.0,
+                color: Colors.pink[50],
+              ),
+            ),
+            Expanded(
+             child: Image(
+               // ทำให้รูปเต็มช่อง
+               fit : BoxFit.fill,
+               height: 100.0,
+               image: AssetImage('asset/img/profile.jpg')),
+
+            )
+          
+          ],
+        )
+      ],
+    ),
+      ],);
   }
 }
